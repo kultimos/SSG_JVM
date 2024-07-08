884c468a0352f86192a48883a8884ad0610a4799
wushuai <659-4gpe145ln@dingtalk.com>
2024-07-08 10:24:23 +0800

    【项目初始化】

---
+++ src/main/java/com/kul/SsgJvmApplication.java
@@ -0,0 +1,13 @@
+package com.kul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsgJvmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsgJvmApplication.class, args);
+    }
+
+}
