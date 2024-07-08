94995c59d4aa2eda3dccf632a487adeacb1d6049
wushuai <659-4gpe145ln@dingtalk.com>
2024-07-08 20:05:17 +0800

    P6:概述&字节码相关面试题

---
+++ src/main/java/com/kul/Test.java
@@ -0,0 +1,16 @@
+package com.kul;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+
+        String str1 = new String("hello") ;
+        String str2 = "hello";
+//        str1.intern();
+        System.out.println(str2 == str1);
+
+//        System.out.println();
+    }
+}
