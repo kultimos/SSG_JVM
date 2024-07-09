80e457353da3bffb61af0aad4183dd84441bf066
wushuai <659-4gpe145ln@dingtalk.com>
2024-07-09 15:01:33 +0800

    P8:几道面试题

--- src/main/java/com/kul/Test.java
+++ src/main/java/com/kul/Test.java
@@ -5,11 +5,9 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-
-        String str1 = new String("hello") ;
-        String str2 = "hello";
-//        str1.intern();
-        System.out.println(str2 == str1);
+        Integer a = 1000;
+        int c = 1000;
+        System.out.println(a==c);
 
 //        System.out.println();
     }
